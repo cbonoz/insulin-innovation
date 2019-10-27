1972033ecdefc5635a8c11843109fe357992ee50
Chris Buonocore <chrisdistrict@gmail.com>
2019-10-26 22:14:24 -0700

    header and carbs object

--- src/components/FoodAccordion.js
+++ src/components/FoodAccordion.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
-
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
 export default class FoodAccordion extends Component {
+
+
     render() {
+        const { results, setFood } = this.props
+
+        const topResults = results.slice(0, Math.min(5, results.length))
+
         return (
-            <div>
-                
+            <div className='food-accordion'>
+                <h1 className='food-accordion-header'>Select Food</h1>
+                <Accordion>
+                    {topResults.map((r, i) => {
+                        return <AccordionItem key={i}>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    <div className='food-accordion-group'>
+                                        {r.group}
+                                    </div>
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                {r.items.map((item, j) => {
+                                    return <div key={j} className='food-accordion-item' onClick={() => {
+                                        const selected = topResults[i].items[j]
+                                        console.log(i, j, selected)
+                                        setFood(selected)
+                                    }}>
+                                        {item.name}
+                                    </div>
+                                })}
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                    })}
+                </Accordion>
+
             </div>
         )
     }
--- src/components/InsulinResult.js
+++ src/components/InsulinResult.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class InsulinResult extends Component {
     render() {
+        const { food } = this.props
+        const { nutrition } = food
+        const { totalCarbs } = nutrition
+
         return (
-            <div>
-                
+            <div className='insulin-result'>
+               <p>{JSON.stringify(food)}</p> 
+               <br/>
+               <p>Carbs: {totalCarbs}</p>
             </div>
         )
     }
